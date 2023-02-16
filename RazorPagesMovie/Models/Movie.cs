01d1c9e8922e578d3a9b5444753de80d67719519
Majid <49441325+Majid1292@users.noreply.github.com>
2023-02-16 09:01:12 -0500

    Validation

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,17 +1,34 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(40, MinimumLength = 4)]
+        [Required]       
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(20, MinimumLength = 4)]
+        [Required]
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(3, 2)")] 
+        [DataType(DataType.Currency)]
+        [Range(1,250)]
         public decimal Price { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(10, MinimumLength = 1)]
+        [Required]
         public string Rating {get; set;}
     }
 }
