4ede80f6f6af08d9873108301ad1666b3a3b01d0
Majid <49441325+Majid1292@users.noreply.github.com>
2023-03-06 10:18:50 -0500

    New component

--- BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Example.razor.g.cs
+++ BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Example.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace BlazorApp.Shared
+namespace BlazorApp.Pages
 {
     #line hidden
     using System;
@@ -82,28 +82,14 @@ using BlazorApp.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/example")]
+    public partial class Example : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 33 "C:\Users\MAFSHAR\WEB315-SEC002-W23\BlazorApp\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
